3157fe6c38ccabba0a49764e578c85af9b6b258a
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-30 15:39:34 -0700

    Update schema definition to include foreign keys for Users/Comments association, implement getComments route

---
+++ database/models/Trip_Schema.js
@@ -0,0 +1,147 @@
+const { DataTypes } = require("sequelize");
+const db = require("../index");
+
+const User = db.define(
+  "User",
+  {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unqiue: true,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      unqiue: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    salt: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    verified: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "pending",
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    profile_pic: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "",
+    },
+    availability: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+      defaultValue: [],
+    },
+  },
+  {
+    tableName: "users",
+    updatedAt: "updated_at",
+    createdAt: "created_at",
+  }
+);
+
+const Trip = db.define(
+  "Trip",
+  {
+    destination: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "",
+    },
+    start_date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    end_date: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "trips",
+    updatedAt: "updated_at",
+    createdAt: "created_at",
+  }
+);
+
+const Activity = db.define(
+  "Activity",
+  {
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "",
+    },
+    description: {
+      type: DataTypes.STRING(1234),
+      allowNull: false,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    duration: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "activities",
+    updatedAt: "updated_at",
+    createdAt: "created_at",
+  }
+);
+
+const Comment = db.define(
+  "Comment",
+  {
+    body: {
+      type: DataTypes.STRING(1234),
+      allowNull: false,
+    },
+    activity_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    tableName: "comments",
+    indexes: [
+      {
+        using: "BTREE",
+        fields: ["activity_id"],
+      },
+    ],
+    updatedAt: "updated_at",
+    createdAt: "created_at",
+  }
+);
+
+Comment.belongsTo(User, { foreignKey: "user_id" });
+User.hasMany(Comment, { as: "user_id" });
+
+module.exports = { User, Comment, Trip, Activity };
--- server/controllers/comments.js
+++ server/controllers/comments.js
@@ -1,8 +1,18 @@
-const models = require("../../database/models");
+const { Comment, User } = require("../../database/models");
 
 module.exports = {
-  getComments: (req, res) => {
-    res.status(200).json("get comments");
+  getComments: async (req, res) => {
+    const { activity_id } = req.params;
+    Comment.findAll({
+      where: {
+        activity_id,
+      },
+      include: User,
+    })
+      .then((comments) => {
+        delete res.status(200).json(comments);
+      })
+      .catch((error) => res.status(404).send(error));
   },
   createComment: (req, res) => {
     res.status(200).json("create comment");
